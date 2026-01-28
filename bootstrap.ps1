119892ff25f087a3f5027fef44f9d954b135cd3a
JMcKeiray <165083197+JMcKeiray@users.noreply.github.com>
2026-01-28 20:48:34 +0100

    Create bootstrap.ps1

---
+++ bootstrap.ps1
@@ -0,0 +1,17 @@
+$dst = "C:\Windows\Temp\stage2.ps1"
+$task = "ProvisionUsersAtBoot"
+
+Invoke-WebRequest `
+  -Uri "https://raw.githubusercontent.com/TWOJ_REPO/stage2.ps1" `
+  -OutFile $dst `
+  -UseBasicParsing
+
+schtasks /create `
+ /tn $task `
+ /tr "powershell.exe -NoProfile -ExecutionPolicy Bypass -File $dst" `
+ /sc onstart `
+ /ru SYSTEM `
+ /rl HIGHEST `
+ /f
+
+Restart-Computer -Force
