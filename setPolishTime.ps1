4af1c19f05cbd95762abdba5f450b0821bb78b33
JMcKeiray <165083197+JMcKeiray@users.noreply.github.com>
2026-02-23 11:50:53 +0100

    dodano write-host

--- setPolishTime.ps1
+++ setPolishTime.ps1
@@ -1,24 +1,26 @@
 # Skrypt
 
-# Ustawienie usługi automatycznej strefy czasowej jako Automatyczna (bez ręcznego startu)
+Write-host Ustawienie usługi automatycznej strefy czasowej jako Automatyczna (bez ręcznego startu)
 sc.exe config tzautoupdate start=auto
 Start-Service tzautoupdate
 
-# Sprawdzenie statusu
+Write-host Sprawdzenie statusu
 sc.exe query tzautoupdate
 
-# Wymuszenie synchronizacji czasu
+Write-host Wymuszenie synchronizacji czasu
 net stop w32time
 net start w32time
 w32tm /resync /force
 
-# Uśpienie komputera po 1h (AC i DC)
+Write-host Uśpienie komputera po 1h (AC i DC)
 powercfg /change standby-timeout-ac 60
 powercfg /change standby-timeout-dc 60
 
-# Wyłączenie ekranu po 45 min (AC i DC)
+Write-host Wyłączenie ekranu po 45 min (AC i DC)
 powercfg /change monitor-timeout-ac 45
 powercfg /change monitor-timeout-dc 45
 
-# Wyłączenie szybkiego uruchamiania
+Write-host Wyłączenie szybkiego uruchamiania
 powercfg /hibernate off
+
+Write-host Zakonczono.
