4aba5597fa0399094a07f9c2fd4b4f8a7535d8b7
JMcKeiray <165083197+JMcKeiray@users.noreply.github.com>
2026-02-23 11:54:13 +0100

    usunieto nawiasy

--- setPolishTime.ps1
+++ setPolishTime.ps1
@@ -12,11 +12,11 @@ net stop w32time
 net start w32time
 w32tm /resync /force
 
-Write-host "Uśpienie komputera po 1h (AC i DC)"
+Write-host "Uśpienie komputera po 1h"
 powercfg /change standby-timeout-ac 60
 powercfg /change standby-timeout-dc 60
 
-Write-host "Wyłączenie ekranu po 45 min (AC i DC)"
+Write-host "Wyłączenie ekranu po 45 min"
 powercfg /change monitor-timeout-ac 45
 powercfg /change monitor-timeout-dc 45
 
