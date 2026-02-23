e0952a257e13706edf84da6f9a41cde16964eb66
JMcKeiray <165083197+JMcKeiray@users.noreply.github.com>
2026-02-23 12:34:28 +0100

    Literowka RenamePC.ps1

--- RenamePC.ps1
+++ RenamePC.ps1
@@ -2,5 +2,5 @@ param(
     [Parameter(Mandatory=$true)]
     [string]$Number
 )
-Write-Host "Zmieniam nazwe omputera na SP6-4870-5780-$Number: "
+Write-Host "Zmieniam nazwe omputera na SP6-4870-5780-$Number"
 Rename-Computer -NewName "SP6-487-5780-$Number" -Force -Restart
