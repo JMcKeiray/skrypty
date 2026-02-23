af6c80aa89ccb81d65633bac7fca87d3e55c23b9
JMcKeiray <165083197+JMcKeiray@users.noreply.github.com>
2026-02-23 12:38:48 +0100

    Powrot do oryginalu RenamePC.ps1

--- RenamePC.ps1
+++ RenamePC.ps1
@@ -1,6 +1,3 @@
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$Number
-)
-Write-Host "Zmieniam nazwe omputera na SP6-4870-5780-$Number"
-Rename-Computer -NewName "SP6-487-5780-$Number" -Force -Restart
+Write-Host "Zmieniam nazwe omputera na SP6-4870-5780-?? "
+$inputNumber = Read-Host "Podaj numer:"
+Rename-Computer -NewName "SP6-487-5780-$inputNumber" -Force -Restart
