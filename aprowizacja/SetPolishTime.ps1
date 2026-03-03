0ee6019824062297a154d47194148223c46a4741
JMcKeiray <165083197+JMcKeiray@users.noreply.github.com>
2026-03-03 16:50:14 +0100

    Update SetPolishTime.ps1

--- aprowizacja/SetPolishTime.ps1
+++ aprowizacja/SetPolishTime.ps1
@@ -1,29 +1,17 @@
 # Skrypt
+Write-Host "Skrypt SetPolishTime v1.0"
 
-Write-host "Ustawienie usługi automatycznej strefy czasowej jako Automatyczna"
+# TODO: Ustawienie na sztywno polskiej strefy czasowej
+Write-Host " - Ustawienie usługi automatycznej strefy czasowej jako Automatyczna"
 sc.exe config tzautoupdate start=auto
 Start-Service tzautoupdate
 
-Write-host "Sprawdzenie statusu"
+Write-Host " - Sprawdzenie statusu"
 sc.exe query tzautoupdate
 
-Write-host "Wymuszenie synchronizacji czasu"
+Write-Host " - Wymuszenie synchronizacji czasu"
 net stop w32time
 net start w32time
 w32tm /resync /force
 
-Write-host "Uśpienie komputera po 1h"
-powercfg /change standby-timeout-ac 60
-powercfg /change standby-timeout-dc 60
-
-Write-host "Wyłączenie ekranu po 45 min"
-powercfg /change monitor-timeout-ac 45
-powercfg /change monitor-timeout-dc 45
-
-Write-host "Wyłączenie szybkiego uruchamiania"
-powercfg /hibernate off
-
-Write-Host "TYMCZASOWO: winget instalacja adobe reader"
-winget install --source winget adobe.acrobat.reader.64-bit
-
-Write-host "Zakonczono."
+Write-Host "Zakonczono SetPolishTime."
